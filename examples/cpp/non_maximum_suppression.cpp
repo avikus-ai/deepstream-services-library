b2d37fdb7b0ca2d1bc31e9155ef8ae327f2f3b20
youngjae-avikus <youngjae.you@avikus.ai>
2022-07-18 22:54:55 +0900

    nms cpp code v0.1.alpha version update
    Currently, nms works well for the car when infer config cluster_mode is 4, but strangely, it does not operate when it is person.

--- examples/cpp/non_maximum_suppression.cpp
+++ examples/cpp/non_maximum_suppression.cpp
@@ -32,6 +32,10 @@ THE SOFTWARE.
 #include <glib.h>
 #include <gst/gst.h>
 #include <gstnvdsmeta.h>
+#include <algorithm>
+#include <numeric>
+#include <NumCpp.hpp>
+#include <cstdint>
 
 #include "DslApi.h"
 
@@ -40,7 +44,7 @@ std::wstring uri_h265(
 
 // Filespecs for the Primary GIE
 std::wstring primary_infer_config_file(
-    L"/opt/nvidia/deepstream/deepstream/samples/configs/deepstream-app/config_infer_primary_nano.txt");
+    L"/opt/nvidia/deepstream/deepstream/samples/configs/deepstream-app/config_infer_nms_test.txt");
 std::wstring primary_model_engine_file(
     L"/opt/nvidia/deepstream/deepstream/samples/models/Primary_Detector_Nano/resnet10.caffemodel_b8_gpu0_fp16.engine");
 std::wstring tracker_config_file(
@@ -50,12 +54,61 @@ uint PGIE_CLASS_ID_VEHICLE = 0;
 uint PGIE_CLASS_ID_BICYCLE = 1;
 uint PGIE_CLASS_ID_PERSON = 2;
 uint PGIE_CLASS_ID_ROADSIGN = 3;
+uint VECTOR_RESERVE_SIZE = 1000;
+uint NMS_CLASS_AGNOSTIC = true;
+std::wstring MATCH_METRIC = L"IOU"; // IOS 
+float MATCH_THRESHOLD = 0.5f;
 
 uint WINDOW_WIDTH = DSL_DEFAULT_STREAMMUX_WIDTH;
 uint WINDOW_HEIGHT = DSL_DEFAULT_STREAMMUX_HEIGHT;
 
 // output file path for the MOT Challenge File Action. 
 std::wstring file_path(L"./mot-challenge-data.txt");
+
+
+class ReportData {
+public:
+    int m_report_count;
+    int m_header_interval;
+
+    ReportData(int count, int interval) : m_report_count(count), m_header_interval(interval) {}
+};
+
+boolean dsl_pph_meter_cb(double* session_fps_averages, double* interval_fps_averages, 
+    uint source_count, void* client_data)
+{
+    // cast the C void* client_data back to a py_object pointer and deref
+    // report_data = cast(client_data, POINTER(py_object)).contents.value
+    ReportData *report_data = static_cast<ReportData*>(client_data);
+
+    // Print header on interval
+    if (report_data->m_report_count % report_data->m_header_interval == 0) {
+        std::wstring header = L"";
+
+        for(int i=0; i<source_count; i++) {  
+            header += L"FPS ";
+            header += std::to_wstring(i);
+            header += L" (AVG)";
+        }
+        std::wcout << header << "\n";
+    }
+    
+    // Print FPS counters
+    std::wstring counters = L"";
+
+    for(int i=0; i<source_count; i++) {
+        counters += std::to_wstring(interval_fps_averages[i]);
+        counters += L" ";
+        counters += std::to_wstring(session_fps_averages[i]);
+    }
+
+    std::wcout << counters << "\n";
+
+    // Increment reporting count
+    report_data->m_report_count += 1;
+    
+    return true;
+}
 
 // 
 // Function to be called on XWindow KeyRelease event
@@ -106,6 +159,60 @@ void state_change_listener(uint old_state, uint new_state, void* client_data)
         << ", new state = " << dsl_state_value_to_string(new_state) << std::endl;
 }
 
+void print_confidence(const std::vector<NvDsObjectMeta*> &obj_array) {
+	for(const auto &x: obj_array) {
+		std::cout << x->confidence << ' ';
+	}
+	std::cout << '\n';
+}
+
+template<typename T>
+void print_1dVector(const std::vector<T> &arr) {
+	for( auto &row : arr) {
+		std::cout << row << ' ';
+	}
+	std::cout << '\n';
+}
+
+template<typename T>
+void print_2dVector(const std::vector<T> &arr) {
+	for( auto &row : arr) {
+		for(auto &col : row)
+			 std::cout << col << ' ';
+		std::cout << '\n';
+	}
+}
+
+bool confidence_compare(const NvDsObjectMeta* u, const NvDsObjectMeta *v) {
+	return u->confidence > v->confidence;
+}
+
+//template<typename T>
+//std::vector<size_t> argsort(const std::vector<T> &array) {
+//    std::vector<size_t> indices(array.size());
+//    std::iota(indices.begin(), indices.end(), 0);
+//    std::sort(indices.begin(), indices.end(),
+//              [&array](int left, int right) -> bool {
+//                  // sort indices according to corresponding array element
+//                  return array[left]->confidence < array[right]->confidence;
+//              });
+//
+//    return indices;
+//}
+
+template<typename T>
+std::vector<uint32_t> argsort(const std::vector<T> &array) {
+    std::vector<uint32_t> indices(array.size());
+    std::iota(indices.begin(), indices.end(), 0);
+    std::sort(indices.begin(), indices.end(),
+              [&array](int left, int right) -> bool {
+                  // sort indices according to corresponding array element
+                  return array[left] < array[right];
+              });
+
+    return indices;
+}
+
 //
 // Custom Pad Probe Handler (PPH) to process the batch-meta for every frame
 //
@@ -115,6 +222,8 @@ uint custom_batch_meta_handler(void* buffer, void* client_data)
     
     NvDsBatchMeta* pBatchMeta = gst_buffer_get_nvds_batch_meta(pGstBuffer);
     
+    // client_data -> Agnostic?
+		
     // For each frame in the batched meta data
     for (NvDsMetaList* pFrameMetaList = pBatchMeta->frame_meta_list; 
         pFrameMetaList; pFrameMetaList = pFrameMetaList->next)
@@ -124,6 +233,11 @@ uint custom_batch_meta_handler(void* buffer, void* client_data)
         if (pFrameMeta != NULL)
         {
             NvDsMetaList* pObjectMetaList = pFrameMeta->obj_meta_list;
+			std::vector<NvDsObjectMeta*> obj_array;
+			std::vector<std::vector<float>> predictions;
+
+			obj_array.reserve(VECTOR_RESERVE_SIZE);
+
             // For each detected object in the frame.
             while (pObjectMetaList)
             {
@@ -135,9 +249,169 @@ uint custom_batch_meta_handler(void* buffer, void* client_data)
 
                 if (pObjectMeta != NULL)
                 {
-                    nvds_remove_obj_meta_from_frame(pFrameMeta, pObjectMeta);
+					obj_array.emplace_back(pObjectMeta);
+					// https://github.com/obss/sahi/blob/91a0becb0d86f0943c57f966a86a845a70c0eb77/sahi/postprocess/combine.py#L17-L40
+					if (NMS_CLASS_AGNOSTIC) {
+						predictions.emplace_back(std::vector<float>{
+									pObjectMeta->rect_params.left,
+									pObjectMeta->rect_params.top,
+									pObjectMeta->rect_params.left + pObjectMeta->rect_params.width,
+									pObjectMeta->rect_params.top + pObjectMeta->rect_params.height, 
+									pObjectMeta->confidence,
+									});
+					}
+					else {
+						predictions.emplace_back(std::vector<float>{
+									pObjectMeta->rect_params.left,
+									pObjectMeta->rect_params.top,
+									pObjectMeta->rect_params.left + pObjectMeta->rect_params.width,
+									pObjectMeta->rect_params.top + pObjectMeta->rect_params.height, 
+									pObjectMeta->confidence,
+									float(pObjectMeta->class_id)
+									});
+					}
+                    // nvds_remove_obj_meta_from_frame(pFrameMeta, pObjectMeta);
                 }
             }
+
+			// print_2dVector(predictions);							
+			// https://dpilger26.github.io/NumCpp/doxygen/html/classnc_1_1_nd_array.html#a9d7045ecdff86bac3306a8bfd9a787eb
+			nc::NdArray<float> nd_predictions{predictions};
+
+//			# we extract coordinates for every
+//			# prediction box present in P
+//    		x1 = predictions[:, 0]
+//			y1 = predictions[:, 1]
+//			x2 = predictions[:, 2]
+//			y2 = predictions[:, 3]
+			
+			auto x1 = nd_predictions(nd_predictions.rSlice(), 0);
+			auto y1 = nd_predictions(nd_predictions.rSlice(), 1);
+			auto x2 = nd_predictions(nd_predictions.rSlice(), 2);
+			auto y2 = nd_predictions(nd_predictions.rSlice(), 3);
+
+			// scores = predictions[:, 4]
+			auto scores = nd_predictions(nd_predictions.rSlice(), 4);
+
+    		// areas = (x2 - x1) * (y2 - y1)
+			auto areas = (x2 - x1) * (y2 - y1);
+			
+			// std::vector<size_t> order = argsort(obj_array);
+			// https://dpilger26.github.io/NumCpp/doxygen/html/classnc_1_1_nd_array.html#a1fb3a21ab9c10a2684098df919b5b440
+			
+			// # sort the prediction boxes in P
+    		// # according to their confidence scores
+		    // order = scores.argsort()
+			std::vector<uint32_t> order = argsort(scores.toStlVector());
+
+			nc::NdArray<nc::uint32> nd_order{order};
+			// std::sort(obj_array.begin(), obj_array.end(), confidence_compare);
+			
+			// # initialise an empty list for
+    		// # filtered prediction boxes
+    		// keep = []
+			std::vector<unsigned int> keep, remove;
+			
+			// return DSL_PAD_PROBE_OK;
+			
+			//while (order.size() > 0) {
+			while (nc::shape(nd_order).size() > 0) {
+				// auto idx = order.back();
+				auto idx = nd_order[-1];
+
+				keep.emplace_back(idx);
+
+				// order.pop_back();
+			    nd_order = nd_order(nd_order.rSlice(), nc::Slice(0,-1));
+				
+				if (nc::shape(nd_order).size() == 0) 
+					break;	
+				
+//				# select coordinates of BBoxes according to
+//		        # the indices in order
+//        		xx1 = torch.index_select(x1, dim=0, index=order)
+//        		xx2 = torch.index_select(x2, dim=0, index=order)
+//        		yy1 = torch.index_select(y1, dim=0, index=order)
+//        		yy2 = torch.index_select(y2, dim=0, index=order)
+				
+				nc::NdArray<nc::uint32> index_other = nd_order;
+				nc::NdArray<nc::uint32> index{idx};
+				
+				auto xx1 = x1[index_other];
+				auto xx2 = x2[index_other];
+				auto yy1 = y1[index_other];
+				auto yy2 = x2[index_other];
+
+//				# find the coordinates of the intersection boxes
+//				xx1 = torch.max(xx1, x1[idx])
+//				yy1 = torch.max(yy1, y1[idx])
+//				xx2 = torch.min(xx2, x2[idx])
+//				yy2 = torch.min(yy2, y2[idx])
+				
+				for(auto it = xx1.begin(); it != xx1.end(); ++it) 
+					if (*it < x1[index].item()) 
+						*it = x1[index].item();
+					
+				for(auto it = yy1.begin(); it != yy1.end(); ++it) 
+					if (*it < y1[index].item()) 
+						*it = y1[index].item();
+								
+				for(auto it = xx2.begin(); it != xx2.end(); ++it) 
+					if (*it > x2[index].item()) 
+						*it = x2[index].item();
+
+				for(auto it = yy2.begin(); it != yy2.end(); ++it) 
+					if (*it > y2[index].item()) 
+						*it = y2[index].item();
+
+//				# find height and width of the intersection boxes
+//				w = xx2 - xx1
+//				h = yy2 - yy1
+				
+				auto w = xx2 - xx1;
+				auto h = yy2 - yy1;
+
+//				# take max with 0.0 to avoid negative w and h
+//				# due to non-overlapping boxes
+//				w = torch.clamp(w, min=0.0)
+//				h = torch.clamp(h, min=0.0)
+				
+				w = nc::clip(w, 0.0f, float(1e9));
+			    h = nc::clip(h, 0.0f, float(1e9));
+
+//				# find the intersection area
+//		        inter = w * h
+				
+				auto inter = w * h;
+
+//				# find the areas of BBoxes according the indices in order
+//        		rem_areas = torch.index_select(areas, dim=0, index=order)i
+				
+				auto rem_areas = areas[index_other];
+
+				if (MATCH_METRIC == L"IOU") {
+					auto _union = (rem_areas - inter) + areas[index].item();
+					auto match_metric_value = inter / _union;
+					auto mask = match_metric_value < MATCH_THRESHOLD;					
+					
+					auto rm_idx = 0;
+					for(auto it = mask.begin(); it != mask.end(); ++it, ++rm_idx) {
+						if (*it == 0)
+							remove.emplace_back(index_other[rm_idx]);
+					}
+					nd_order = nd_order[mask];
+				}
+				else if (MATCH_METRIC == L"IOS") {
+
+				}
+				else {
+					return -1; // Change assert
+				}			
+			}
+			
+			for (auto &x: remove) {
+				nvds_remove_obj_meta_from_frame(pFrameMeta, obj_array[x]);
+			}
         }
     }
     return DSL_PAD_PROBE_OK;
@@ -150,6 +424,10 @@ int main(int argc, char** argv)
     // Since we're not using args, we can Let DSL initialize GST on first call    
     while(true) 
     {    
+		ReportData report_data(0, 12);
+
+        retval = dsl_pph_meter_new(L"meter-pph", 1, dsl_pph_meter_cb, &report_data);
+        if (retval != DSL_RESULT_SUCCESS) break;
 
         //```````````````````````````````````````````````````````````````````````````````````
         // Create a new Custom Pad Probe Handler. 
@@ -166,7 +444,7 @@ int main(int argc, char** argv)
 
         // New Primary GIE using the filespecs above, with interval and Id
         retval = dsl_infer_gie_primary_new(L"primary-gie", 
-            primary_infer_config_file.c_str(), primary_model_engine_file.c_str(), 4);
+            primary_infer_config_file.c_str(), primary_model_engine_file.c_str(), 0);
         if (retval != DSL_RESULT_SUCCESS) break;
 
         // New IOU Tracker, setting max width and height of input frame
@@ -180,6 +458,9 @@ int main(int argc, char** argv)
 
         // New OSD with text, clock and bbox display all enabled. 
         retval = dsl_osd_new(L"on-screen-display", true, true, true, false);
+        if (retval != DSL_RESULT_SUCCESS) break;
+
+        retval = dsl_osd_pph_add(L"on-screen-display", L"meter-pph", DSL_PAD_SINK);
         if (retval != DSL_RESULT_SUCCESS) break;
 
         // New Overlay Sink, 0 x/y offsets and same dimensions as Tiled Display
